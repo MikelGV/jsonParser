5213eb75b7e3fdb772b8ea16e3a0b1d7e70035fa
MikelGv <mikelgaldosv@gmail.com>
2024-08-15 16:46:10 +0200

    lexer/parser file and folerds created

---
+++ lexer/lexer.go
@@ -0,0 +1,5 @@
+package lexer
+
+type Lexer struct {
+    // Here should go something but i don't know what right now
+}
---
+++ parser/parser.go
@@ -0,0 +1,5 @@
+package parser
+
+type Parser struct {
+    // Here should go something but i don't know what.
+}
